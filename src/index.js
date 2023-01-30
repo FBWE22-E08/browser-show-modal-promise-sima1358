b6a4561ab0917eec805129c112e1f63814efc9e0
dci-student <simaafshar1@gmail.com>
2023-01-30 14:28:25 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./css/style.css"; //importing css
+ const paragraph = document.querySelector('.modal')
+const paragraphPromise = () => {
+    return new Promise ((resolved) => {
+        setTimeout (() => {
+            resolved ( "block")
+        }, 60000)
+
+        })
+};
+
+const resolvedPromise = async ()=>{
+    let result = await paragraphPromise ();
+    paragraph.style.display = result
+};
+
+resolvedPromise();
